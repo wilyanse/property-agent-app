39af44b5bce35688d7fbd6c8072da24d918a0ff1
wilyanse <willjose51@gmail.com>
2026-01-16 09:15:54 +0800

    fix: cors issue by enabling CORS first before routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,17 @@ import express from "express";
 import agentRoutes from "./routes/agent.routes";
 import cors from "cors";
 
+
 const app = express();
-app.use(express.json());
-app.use("/agents", agentRoutes);
 
 // Enable CORS
 app.use(cors({
-  origin: "http://localhost:5173", // allow frontend origin
+  origin: "*", // allow frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
+
+app.use(express.json());
+app.use("/agents", agentRoutes);
 
 // health check to see if server is up and running
 app.get("/health", (_req, res) => {
